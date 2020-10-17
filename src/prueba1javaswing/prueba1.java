89b02964c5813d5b5d5e0cf1277fc31a9f46a74c
AlexCampos98 <72565364+AlexCampos98@users.noreply.github.com>
2020-10-17 19:39:12 +0200

    Creado el codigo completo del ejercicio. Final

--- src/prueba1javaswing/prueba1.java
+++ src/prueba1javaswing/prueba1.java
@@ -50,7 +50,6 @@ public class prueba1 extends javax.swing.JFrame
 
         btn1.addActionListener(new ActionListener()
         {
-            @Override
             public void actionPerformed(ActionEvent e)
             {
                 JOptionPane.showMessageDialog(null, "Prueba de mensaje", "Ventana 1", 1);
@@ -59,68 +58,127 @@ public class prueba1 extends javax.swing.JFrame
 
         btn2.addActionListener(new ActionListener()
         {
-            @Override
             public void actionPerformed(ActionEvent e)
             {
                 JOptionPane.showMessageDialog(null, "Advertencia Loco", "Gran advertencia", 2);
             }
         });
-        
+
         btn3.addActionListener(new ActionListener()
         {
-            @Override
             public void actionPerformed(ActionEvent e)
             {
                 JOptionPane.showMessageDialog(null, "La has liado", "Error", 0);
             }
         });
-        
+
         btn4.addActionListener(new ActionListener()
         {
-            @Override
             public void actionPerformed(ActionEvent e)
             {
-                JOptionPane.showInputDialog(null, "Introduce informacion", "Entrada de datos", 1);
-            }
-        });
-        
-        btn5.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                
-            }
-        });
-        
-        btn6.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                
-            }
-        });
-        
-        btn7.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                
-            }
-        });
-        
-        btn8.addActionListener(new ActionListener()
-        {
-            @Override
-            public void actionPerformed(ActionEvent e)
-            {
-                
+                String informacion = JOptionPane.showInputDialog(null, "Introduce informacion", "Entrada de datos", 1);
+                System.out.println(informacion);
             }
         });
 
-//        pack();
+        btn5.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                String nDoblar = JOptionPane.showInputDialog(null, "Introduce un numero: ", "Multiplicador x2", -1);
+                boolean resultado;
+                Integer numero = 0;
+
+                try
+                {
+                    numero = Integer.parseInt(nDoblar);
+                    resultado = true;
+                    numero = numero * 2;
+                } catch (NumberFormatException eNumero)
+                {
+                    resultado = false;
+                }
+
+                if (resultado)
+                {
+                    JOptionPane.showMessageDialog(null, "El resultado es: " + numero * 2, "Rersultado", 1);
+                } else
+                {
+                    JOptionPane.showMessageDialog(null, "No has introducido correctamente el numero, esto es lo que has introducido: " + nDoblar, "Error del sistema", 2);
+                }
+
+            }
+        });
+
+        btn6.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                String[] opciones =
+                {
+                    "Este", "Este no", "Mejor no", "Elijo", "Ninguno"
+                };
+                String opcion = (String) JOptionPane.showInputDialog(null, "Seleciona una opcion:", "Menu de seleccion", JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[2]);
+                JOptionPane.showMessageDialog(null, opcion, "Has seleccionado", 1);
+
+            }
+        });
+
+        btn7.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                String[] opciones =
+                {
+                    "Opcion 1", "Opcion 2", "Opcion 3"
+                };
+
+                int opcion = JOptionPane.showOptionDialog(null, "Pulsa uno de los botones", "Elije entre una de las opciones", WIDTH, HEIGHT, null, opciones, opciones[0]);
+
+                String mensaje = "";
+
+                switch (opcion)
+                {
+                    case 0:
+                        mensaje = "Opcion 1";
+                        break;
+                    case 1:
+                        mensaje = "Opcion 2";
+                        break;
+                    case 2:
+                        mensaje = "Opcion 3";
+                        break;
+                }
+
+                JOptionPane.showMessageDialog(null, mensaje, "La opcion escogida es", 1);
+            }
+        });
+
+        btn8.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                int opcion = JOptionPane.showConfirmDialog(null, "¿Se acabara el Covid rapido?", "Pregunta dificil", JOptionPane.YES_NO_CANCEL_OPTION,2);
+
+                String mensaje = "";
+
+                switch (opcion)
+                {
+                    case 0:
+                        mensaje = "Si";
+                        break;
+                    case 1:
+                        mensaje = "No";
+                        break;
+                    case 2:
+                        mensaje = "Cancelar";
+                        break;
+                }
+
+                System.out.println("La opcion elegida es: " + mensaje);
+            }
+        });
+
         setVisible(true);
     }
 
@@ -150,10 +208,9 @@ public class prueba1 extends javax.swing.JFrame
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-   
-        /**
-         * @param args the command line arguments
-         */
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String args[])
     {
         /* Set the Nimbus look and feel */
